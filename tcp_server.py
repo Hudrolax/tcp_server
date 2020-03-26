8623593dc89640105766a67f90424ee6f07ada93
Hudrolax <hudro795@gmail.com>
2020-03-26 23:02:53 +0300

    init golden1 branch

--- tcp_server.py
+++ tcp_server.py
@@ -6,6 +6,7 @@ import psutil
 from time import sleep
 from datetime import datetime
 import threading
+import subprocess
 
 OWN_TCP_SERVER_ADDRESS = ('', 8686)
 cpu_percent = 0
@@ -19,7 +20,11 @@ def DifferenceBetweenDate(date1,date2):
 	hours = divmod(days[1], 3600)               # Use remainder of days to calc hours
 	minutes = divmod(hours[1], 60)                # Use remainder of hours to calc minutes
 	seconds = divmod(minutes[1], 1)               # Use remainder of minutes to calc seconds
-	return "%d days, %d hours, %d mins %d sec" % (days[0], hours[0], minutes[0], seconds[0]) 
+	return "%d days, %d hours, %d mins %d sec" % (days[0], hours[0], minutes[0], seconds[0])
+
+def DifferenceBetweenDateSec(date1,date2):
+	duration = date2 - date1
+	return duration.total_seconds() # Total number of seconds between dates	 
 
 def ClerStr(str_):
 	str_ = str(str_)
@@ -27,7 +32,57 @@ def ClerStr(str_):
 	str_ = str_.replace("b'",'')
 	str_ = str_.replace("'",'')
 	return str_
-	
+
+def GetAndUpdate():
+	subprocess.call("GetAndUpdate.bat")
+
+def modification_date(filename):
+		t = os.path.getmtime(filename)
+		return datetime.fromtimestamp(t)
+
+def last_file_in_list(file_list):
+		backups_time = []
+		for file in file_list:
+			backups_time.append(modification_date(file))
+		return max(backups_time)	
+
+def get_last_update_name():
+		dirs = os.listdir()
+		file_list = []
+		for file in dirs:
+			if file.find('update_log_')>-1:
+				file_list.append(file)
+		last_date = last_file_in_list(file_list)
+		for file in dirs:
+			if modification_date(file) == last_date:
+				return file			
+		return None	
+
+def get_last_update_date():
+		dirs = os.listdir()
+		file_list = []
+		for file in dirs:
+			if file.find('update_log_')>-1:
+				file_list.append(file)
+		return last_file_in_list(file_list)		
+
+def GetLog():
+	last_update_name = get_last_update_name()
+	if last_update_name != None:
+		#if DifferenceBetweenDateSec(get_last_update_date(),datetime.now()) < 1800:
+		try:
+			f = open(last_update_name,'r')
+			log = f.read()
+			f.close()
+			return log
+		except:
+			return f'Error open file {last_update_name}'
+		#else:
+		#	return "There's no actual log"	
+					
+	else:
+		return "There's no log"			
+
 def TCPServer():
 	# Настраиваем сокет
 	global cpu_percent
@@ -71,7 +126,13 @@ def TCPServer():
 		elif data == 'virtual_memory':
 			answer = str(virtual_memory)
 		elif data == 'uptime':
-			answer = str(DifferenceBetweenDate(starttime,datetime.now()))									
+			answer = str(DifferenceBetweenDate(starttime,datetime.now()))
+		elif data == 'update_base':
+			GetAndUpdateThread = threading.Thread(target=GetAndUpdate, args=(), daemon=True)
+			GetAndUpdateThread.start()
+			answer = 'update is started'
+		elif data == 'get_log':
+			answer = GetLog()						
 				
 		connection.send(bytes(answer, encoding='UTF-8'))
 		connection.close()
